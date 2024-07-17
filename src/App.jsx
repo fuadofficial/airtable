9affecbd0a3d9e7529f60718abbe66f6eb936738
fuad <ahmadfuadoorakam@gmail.com>
2024-07-17 05:36:35 +0530

    useEffect some

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,12 @@ import "./App.css";
 import { Counter } from "./component/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const Increment = () => {
-    if (count < 10) {
-      setCount((prev) => prev + 1);
-    }
-  };
-  const Decrement = () => {
-    if (count > 0) {
-      setCount((prev) => prev - 1);
-    }
-  };
+  const [show, setShow] = useState(false);
+
   return (
     <div className="couter">
-      <Counter count={count} Increment={Increment} Decrement={Decrement} />
+      <button onClick={() => setShow(!show)}>Show/Hide</button>
+      {show && <Counter />}
     </div>
   );
 }
--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,4 +1,27 @@
-export const Counter = ({ count, Increment, Decrement }) => {
+import { useEffect, useState } from "react";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  useEffect(() => {
+    console.log("mounding");
+    console.log("Upadating..." + count);
+    
+  }, [count]);
+
+  const Increment = () => {
+    if (count < 10) {
+      setCount((prev) => prev + 1);
+    }
+  };
+
+  const Decrement = () => {
+    if (count > 0) {
+      setCount((prev) => prev - 1);
+    }
+  };
+
   return (
     <div>
       <h1>{count}</h1>
