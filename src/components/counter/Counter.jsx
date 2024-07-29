c7a99d2785071de1473d90587ecb5a51eabc5971
fuad <ahmadfuadoorakam@gmail.com>
2024-07-29 19:24:16 +0530

    created counter project with useReducer

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,15 +1,32 @@
 import React, { useReducer } from 'react'
+import './Counter.css'
 
 const counterReducer = (state, action) => {
     switch (action.type) {
         case "increment": {
-            return count = action.payload
+            return {
+                count: state.count + 1
+            }
         }
         case "decrement": {
-            return count = action.payload 
+            return {
+                count: state.count - 1
+            }
         }
         case "reset": {
-            return count 
+            return {
+                count: 0
+            }
+        }
+        case "onincrement_by": {
+            return {
+                count: state.count + action.payload
+            }
+        }
+        case "ondecrement_by": {
+            return {
+                count: state.count + action.payload
+            }
         }
         default:
             break;
@@ -21,21 +38,18 @@ const Counter = () => {
         count: 0,
     })
 
-    // const onIncrement = () => {
-    //     dispatch((prev) => prev + 1)
-    // }
-    // const onDecrement = () => {
-    //     dispatch((prev) => prev - 1)
-    // }
-    // const onReset = () => {
-    //     dispatch(0)
-    // }
     return (
-        <div>
-            <h1>{state.count}</h1>
-            <button onClick={() => dispatch({ type: 'increment', payload: 1 })}>Decrement</button>
-            <button onClick={() => dispatch({ type: 'decrement', payload: -1 })}>INcrement</button>
-            <button onClick={() => dispatch({ type: 'reset', payload: 0 })}>Reset</button>
+        <div className='counter-container'>
+            <div className="heading">
+                <h1>{state.count}</h1>
+            </div>
+            <div className="buttons">
+                <button onClick={() => dispatch({ type: 'increment' })}> INcrement</button>
+                <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+                <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+                <button onClick={() => dispatch({ type: 'onincrement_by', payload: 10 })}>on Increment By</button>
+                <button onClick={() => dispatch({ type: 'ondecrement_by', payload: -10 })}>on Decrement By</button>
+            </div>
         </div>
     )
 }
