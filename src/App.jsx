9c7c2fdd9958d00e71972ca8df2e9591ef5c88ec
fuad <ahmadfuadoorakam@gmail.com>
2024-07-31 20:35:35 +0530

    created router project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import Counter from './components/counter/Counter'
-import Search from './components/Search'
+// import Counter from './components/counter/Counter'
+// import Search from './components/Search'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Header from './components/HomeProject/Header/Header'
 
 const App = () => {
     return (
         <div>
+            {/* search project */}
             {/* <Search /> */}
-            <Counter />
+
+            {/* counter porject */}
+            {/* <Counter /> */}
+
+            {/* ProductProject */}
+
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/product' element={<Product />} />
+                </Routes>
+            </Router>
+
         </div>
     )
 }
