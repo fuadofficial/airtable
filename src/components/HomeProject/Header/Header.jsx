25b5608af36cc177b5bb9c13ff67c4f6e7538dd3
fuad <ahmadfuadoorakam@gmail.com>
2024-08-01 08:40:42 +0530

    routing......

--- src/components/HomeProject/Header/Header.jsx
+++ src/components/HomeProject/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/products'}>Products</Link>
+            <Link to={'/login'}>Login</Link>
+        </div>
+    )
 }
 
 export default Header
