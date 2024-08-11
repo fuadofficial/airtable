119b5fdf8583b0cd24be65705bbe64f40c8ebcb0
fuad <ahmadfuadoorakam@gmail.com>
2024-08-11 13:42:53 +0530

    add image

--- src/components/HomeProject/Image/Image.jsx
+++ src/components/HomeProject/Image/Image.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Image.css'
 
 const Image = () => {
   return (
-    <div>
-        <h1><img src="https://www.shutterstock.com/image-photo/beautiful-aerial-view-downtown-vancouver-600nw-2244151975.jpg" alt="" /></h1>
+    <div className='image'>
+      <img src="https://www.shutterstock.com/image-photo/beautiful-aerial-view-downtown-vancouver-600nw-2244151975.jpg" alt="" />
     </div>
   )
 }
