88521c905b0640ec431f0ebd3e1b72ba7e55d412
fuad <ahmadfuadoorakam@gmail.com>
2024-08-12 06:06:48 +0530

    s

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,20 @@
-import { useState } from "react"
+import { lazy, Suspense, useState } from "react"
 import Container from "../layout/Container"
-import Image from "../components/HomeProject/Image/Image"
+
+const Image = lazy(() => import("../components/HomeProject/Image/Image"))
 
 const About = () => {
 	const [image, setImage] = useState(false)
 	return (
 		<Container>
-
 			<div>
 				<h1>This is About page</h1>
 				<button onClick={() => setImage(!image)}>Image</button>
-				{image && <Image />}
+				{image &&
+					<Suspense fallback={<h1>Loding.....</h1>}>
+						<Image />
+					</Suspense>
+				}
 			</div>
 		</Container>
 	)
